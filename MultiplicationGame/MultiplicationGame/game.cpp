1ac09eccb2557020697c5f0c09f4a2983322f3c8
Sasha Ocheev <OcheevSasha@gmail.com>
2016-05-27 19:36:59 +0300

    незначительные изменения

--- MultiplicationGame/MultiplicationGame/game.cpp
+++ MultiplicationGame/MultiplicationGame/game.cpp
@@ -5,12 +5,12 @@
 
 int CalculateWinner(int limit)
 {
-	int winner = 1;
-	while (limit > 1)
+	bool doesFirstPlayerWins = false;
+	do
 	{
-		limit = static_cast<int>(ceil(limit / ((winner) ? 5.0 : 2.0)));
-		winner = abs(winner - 1);
-	}
+		limit = static_cast<int>(ceil(limit / ((doesFirstPlayerWins) ? 2.0 : 5.0)));
+		doesFirstPlayerWins = !doesFirstPlayerWins;
+	} while (limit > 1);
 
-	return winner + 1;
+	return (doesFirstPlayerWins) ? 1 : 2;
 }
